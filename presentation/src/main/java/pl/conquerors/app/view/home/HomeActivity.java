1ad2bd3c219e38789249ba0127c6f228aeb11dd0
ALUGratka <gratkowska.ala@gmail.com>
2020-03-30 23:03:57 +0200

    Home activity buttons

--- presentation/src/main/java/pl/conquerors/app/view/home/HomeActivity.java
+++ presentation/src/main/java/pl/conquerors/app/view/home/HomeActivity.java
@@ -3,13 +3,21 @@ package pl.conquerors.app.view.home;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 import pl.conquerors.app.R;
+import pl.conquerors.app.base.BaseActivity;
 import pl.conquerors.app.navigation.Navigator;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseActivity {
+
+    @BindView(R.id.homePrizeButton)
+    Button mHomePrizeButton;
+
+    @BindView(R.id.homeCreatorButton)
+    Button mHomeCreatorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +38,15 @@ public class HomeActivity extends AppCompatActivity {
     @OnClick(R.id.homeCreatorButton)
     public void onCreatorButtonClicked() {
         Navigator.startCreator(this);;
+    }
+
+    @Override
+    public void showLoading() {
+        
+    }
+
+    @Override
+    public void hideLoading() {
+
     }
 }
