24e5643aed0cacf94451c36bb3254c503fde8884
ALUGRatka <gratkowska@gmail.com>
2020-04-15 11:02:21 +0200

    My profile first layout

---
+++ Client/presentation/src/main/java/pl/conquerors/app/view/profile/my/MyProfileActivity.java
@@ -0,0 +1,19 @@
+package pl.conquerors.app.view.profile.my;
+
+import pl.conquerors.app.base.BaseActivity;
+
+public class MyProfileActivity extends BaseActivity implements MyProfileView {
+
+
+
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+}
---
+++ Client/presentation/src/main/java/pl/conquerors/app/view/profile/my/MyProfileView.java
@@ -0,0 +1,6 @@
+package pl.conquerors.app.view.profile.my;
+
+import pl.conquerors.app.base.BaseView;
+
+public interface MyProfileView extends BaseView {
+}
