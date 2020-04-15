a31886d84067e3ed091fc7aea748c4a672e6e913
ALUGRatka <gratkowska@gmail.com>
2020-04-15 15:51:48 +0200

    My profile layout, menu navigation to my profile

--- Client/presentation/src/main/java/pl/conquerors/app/view/home/HomeActivity.java
+++ Client/presentation/src/main/java/pl/conquerors/app/view/home/HomeActivity.java
@@ -3,15 +3,20 @@ package pl.conquerors.app.view.home;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 import pl.conquerors.app.R;
 import pl.conquerors.app.base.BaseActivity;
+import pl.conquerors.app.base.BaseView;
 import pl.conquerors.app.navigation.Navigator;
+import pl.conquerors.app.util.DialogUtil;
 
-public class HomeActivity extends BaseActivity {
+public class HomeActivity extends BaseActivity implements HomeView {
 
     @BindView(R.id.homePrizeButton)
     Button mHomePrizeButton;
@@ -19,15 +24,35 @@ public class HomeActivity extends BaseActivity {
     @BindView(R.id.homeCreatorButton)
     Button mHomeCreatorButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-    }
+    @BindView(R.id.nav_view)
+    NavigationView mNavigationView;
+
+    private HomePresenter homePresenter;
+
+    private NavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            homePresenter.onDrawerItemSelected(menuItem);
+            return true;
+        }
+    };
 
     public static Intent getStartingIntents(final Context context){
         final Intent startingIntent = new Intent(context, HomeActivity.class);
         return startingIntent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mNavigationView.setNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        homePresenter = new HomePresenter();
+        homePresenter.setmView(this);
+
+        homePresenter.created();
     }
 
     @OnClick(R.id.homePrizeButton)
@@ -47,6 +72,32 @@ public class HomeActivity extends BaseActivity {
 
     @Override
     public void hideLoading() {
+
+    }
+
+
+    @Override
+    public void setNavigationButtonsVisibility(boolean isLoggedIn) {
+
+    }
+
+    @Override
+    public void showMyProfile() {
+        Navigator.startMyProfile(this);
+    }
+
+    @Override
+    public void showLogout() {
+
+    }
+
+    @Override
+    public boolean isDrawerOpen() {
+        return false;
+    }
+
+    @Override
+    public void closeDrawer() {
 
     }
 }
---
+++ Client/presentation/src/main/java/pl/conquerors/app/view/home/HomeView.java
@@ -0,0 +1,14 @@
+package pl.conquerors.app.view.home;
+
+import pl.conquerors.app.base.BaseView;
+
+public interface HomeView extends BaseView {
+
+    void setNavigationButtonsVisibility(boolean isLoggedIn);
+
+    void showMyProfile();
+    void showLogout();
+
+    boolean isDrawerOpen();
+    void closeDrawer();
+}
