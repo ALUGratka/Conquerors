40300644254bdeafff279dad3209f767aa147ea4
Ania <anula.le@wp.pl>
2020-06-04 14:07:02 +0200

    tile based map

--- Client/presentation/src/main/java/pl/conquerors/app/view/gameMap/GameMapCanvas.java
+++ Client/presentation/src/main/java/pl/conquerors/app/view/gameMap/GameMapCanvas.java
@@ -1,64 +1,76 @@
 package pl.conquerors.app.view.gameMap;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.Random;
 
 import pl.conquerors.app.R;
 
-public class GameMapCanvas extends SurfaceView {
-    private Bitmap bmp;
-    private SurfaceHolder holder;
+public class GameMapCanvas extends View {
+
+    private final int TILE_SIZE = 100;
+    private static int[][] tileMap;
+    private static int rows, columns;
+    private Drawable box;
+    Paint p;
+    Random random;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public GameMapCanvas(Context context) {
         super(context);
-        holder = getHolder();
+        box = getResources().getDrawable(R.mipmap.skarb, null);
+        p = new Paint();
+        random = new Random();
+        createTilemap();
+    }
 
-        holder.addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder surfaceholder) {
-                Canvas canvas = holder.lockCanvas(null);
-                draw(canvas);
-                holder.unlockCanvasAndPost(canvas);
+    public void createTilemap(){
+        tileMap = new int[30][20];
+        rows = tileMap.length;
+        columns = tileMap[1].length;
+        Random r = new Random();
+
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                tileMap[i][j] = r.nextInt(2);
             }
+        }
+    }
 
-            @Override
-            public void surfaceChanged(SurfaceHolder surfaceholder, int format, int width, int height) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder surfaceholder) {
-
-            }
-        });
-        bmp = BitmapFactory.decodeResource(getResources(), R.mipmap.skarb);
+    public GameMapCanvas(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawColor(Color.GREEN);
-        bmp = replaceColor(bmp);
-        canvas.drawBitmap(bmp, 300, 400, null);
-        canvas.drawBitmap(bmp, 100, 400, null);
-        canvas.drawBitmap(bmp, 100, 100, null);
-    }
 
-    public Bitmap replaceColor(Bitmap src) {
-        if (src == null)
-            return null;
-        int width = src.getWidth();
-        int height = src.getHeight();
-        int[] pixels = new int[width * height];
-        src.getPixels(pixels, 0, width, 0, 0, width, height);
-        for (int x = 0; x < pixels.length; ++x) {
-            if(pixels[x] == Color.WHITE) pixels[x] = 0;
+
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+
+                int pos_i = TILE_SIZE * i;
+                int pos_j = TILE_SIZE * j;
+
+                int r  = random.nextInt(10);
+                if(r%10!=1) {
+                    p.setColor(Color.GREEN);
+                    canvas.drawRect(pos_i, pos_j, pos_i + TILE_SIZE, pos_j + TILE_SIZE, p);
+                }
+                else {
+                    box.setBounds(pos_i, pos_j, pos_i + TILE_SIZE, pos_j+TILE_SIZE);
+                    box.draw(canvas);
+                    System.out.println("pos_i:"+pos_i+" pos_j:"+pos_j+" right:"+(pos_i+TILE_SIZE)+" bottom:"+ (pos_j+TILE_SIZE));
+                }
+            }
         }
-        return Bitmap.createBitmap(pixels, width, height, Bitmap.Config.ARGB_8888);
     }
 }
