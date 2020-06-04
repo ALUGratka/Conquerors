1a3eac415beadd57fd72864cc109f79c570aaaf5
Ania <anula.le@wp.pl>
2020-06-04 12:33:53 +0200

    delete white color

--- Client/presentation/src/main/java/pl/conquerors/app/view/gameMap/GameMapCanvas.java
+++ Client/presentation/src/main/java/pl/conquerors/app/view/gameMap/GameMapCanvas.java
@@ -43,7 +43,22 @@ public class GameMapCanvas extends SurfaceView {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.GREEN);
+        bmp = replaceColor(bmp);
         canvas.drawBitmap(bmp, 300, 400, null);
         canvas.drawBitmap(bmp, 100, 400, null);
+        canvas.drawBitmap(bmp, 100, 100, null);
+    }
+
+    public Bitmap replaceColor(Bitmap src) {
+        if (src == null)
+            return null;
+        int width = src.getWidth();
+        int height = src.getHeight();
+        int[] pixels = new int[width * height];
+        src.getPixels(pixels, 0, width, 0, 0, width, height);
+        for (int x = 0; x < pixels.length; ++x) {
+            if(pixels[x] == Color.WHITE) pixels[x] = 0;
+        }
+        return Bitmap.createBitmap(pixels, width, height, Bitmap.Config.ARGB_8888);
     }
 }
